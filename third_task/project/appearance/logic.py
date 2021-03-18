5e8ea0db434fe3b60f32f7e1a7e950c6e4600b98
yo <nikita.peretolchin@gmail.com>
2021-03-18 22:56:17 +0300

    Changed the procedure of counting the total time

--- third_task/project/appearance/logic.py
+++ third_task/project/appearance/logic.py
@@ -5,7 +5,7 @@ def get_interval_from_list(lst: list) -> list:
 def get_joint_interval(t1: tuple, t2: tuple) -> tuple:
     start = t2[0] if t1[0] <= t2[0] else t1[0]
     end = t1[1] if t1[1] <= t2[1] else t2[1]
-    return (start, end) if start < end else None
+    return (start, end) if start <= end else None
 
 
 def get_all_joint_intervals(l1: list, l2: list) -> list:
@@ -18,13 +18,24 @@ def get_all_joint_intervals(l1: list, l2: list) -> list:
     return joint_intervals
 
 
-def appearance(intervals: dict):
-    lesson_intervals = get_interval_from_list(intervals['lesson'])
-    pupil_intervals = get_interval_from_list(intervals['pupil'])
-    tutor_intervals = get_interval_from_list(intervals['tutor'])
-
-    joint_intervals = get_all_joint_intervals(lesson_intervals, pupil_intervals)
-    joint_intervals = get_all_joint_intervals(joint_intervals, tutor_intervals)
-
-    total_time = sum([i[1] - i[0] for i in joint_intervals])
+def get_total_crossing_time(lst: list) -> int:
+    lst.sort()
+    prev = lst[0]
+    total_time = 0
+    for curr in lst[1:]:
+        if prev[1] > curr[0]:
+            prev = [prev[0], max(prev[1], curr[1])]
+        else:
+            total_time += prev[1] - prev[0]
+            prev = curr
+    total_time += prev[1] - prev[0]
     return total_time
+
+
+def appearance(intervals: dict):
+    joint_intervals = get_interval_from_list(intervals['lesson'])
+    for intervals_ in (intervals['pupil'], intervals['tutor']):
+        intervals_ = get_interval_from_list(intervals_)
+        joint_intervals = get_all_joint_intervals(joint_intervals, intervals_)
+
+    return get_total_crossing_time(joint_intervals)
