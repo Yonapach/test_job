66a873c7d3182e124838261b79b80517606af1bb
yo <nikita.peretolchin@gmail.com>
2021-03-18 19:48:44 +0300

    Added redirect from start page to api

--- third_task/project/project/urls.py
+++ third_task/project/project/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/appearance')),
     path('api/', include('appearance.urls')),
 ]
